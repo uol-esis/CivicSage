f8d872ab9b4baa5a423e70cb3b6f409a65c5858f
Jonas Pohl <git@jaypi4c.de>
2026-03-21 13:55:35 +0100

    Initial setup

---
+++ deployment/data/createGarageToml.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cat > garage.toml <<EOF
+metadata_dir = "/var/lib/garage/meta"
+data_dir = "/var/lib/garage/data"
+db_engine = "sqlite"
+
+replication_factor = 1
+
+rpc_bind_addr = "[::]:3901"
+rpc_public_addr = "127.0.0.1:3901"
+rpc_secret = "$(openssl rand -hex 32)"
+
+[s3_api]
+s3_region = "garage"
+api_bind_addr = "[::]:3900"
+root_domain = ".s3.garage.localhost"
+
+[admin]
+api_bind_addr = "[::]:3903"
+admin_token = "$(openssl rand -base64 32)"
+metrics_token = "$(openssl rand -base64 32)"
+EOF
---
+++ deployment/deploy.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+mkdir -p data
+
+curl -o data/createGarageToml.sh https://raw.githubusercontent.com/uol-esis/civicsage/main/deployment/data/createGarageToml.sh
+chmod +x data/createGarageToml.sh
+data/createGarageToml.sh
+
+
+
+curl -o docker-compose.yaml https://raw.githubusercontent.com/uol-esis/civicsage/main/deployment/docker-compose.yaml
+
+
+touch .env
+echo "MB_GENERATED_KEY=<replace me>" >> .env
+ 
+if [ "$secured" = true ]; then
+  mkdir -p data/realms
+
+  # download file from github.com/uol-esis/th1nk/blob/main/deployment/docker-compose-secured.yaml
+  curl -o docker-compose.yaml https://raw.githubusercontent.com/uol-esis/th1nk/main/deployment/docker-compose-secured.yaml
+
+  # download file for keycloak realm at github.com/uol-esis/th1nk/blob/main/deployment/data/realms/th1nk-realms.json
+  curl -o data/realms/th1nk-realms.json https://raw.githubusercontent.com/uol-esis/th1nk/main/deployment/data/realms/th1nk-realms.json
+
+  docker compose up -d frontend db metabase keycloak
+
+else
+  echo "Please note that this startup does not secure the application. Please read the documentation for more information on how to secure your deployment."
+  
+  # download file from github.com/uol-esis/th1nk/blob/main/deployment/docker-compose.yaml
+  curl -o docker-compose.yaml https://raw.githubusercontent.com/uol-esis/th1nk/main/deployment/docker-compose.yaml
+
+  docker compose up -d frontend db metabase
+  
+fi
+
+echo "Please configure Metabase at http://localhost:3000. (Further information can be found in the documentation.)"
+echo "Database configuration: https://www.metabase.com/docs/v0.57/databases/connections/postgresql.html. The DB credentials can be found in the backend/docker/docker-compose.yaml file."
+echo "Type: postgres, Username: example, Password: example, Host: db, Port: 5432, Database name: th1nk, Schemas: all except th1-internal"
+echo "Obtaining API key with Administrator permissions: https://www.metabase.com/docs/v0.57/people-and-groups/api-keys"
+echo "Provide the obtained API key as an environment variable named MB_GENERATED_KEY via the '.env' file."
+read -p "Press enter to continue after configuration"
+
+docker compose up -d backend
+
+echo "Th1nk is now running. You can access the frontend at http://localhost"
+echo "To stop the application, run 'docker compose down'"
