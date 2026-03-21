2746e68b054fb45d353a87c96123d14e785219df
Jonas Pohl <git@jaypi4c.de>
2026-03-21 14:22:45 +0100

    Reverting back to full garage.toml

--- deployment/data/createGarageToml.sh
+++ deployment/data/createGarageToml.sh
@@ -16,6 +16,14 @@ s3_region = "garage"
 api_bind_addr = "[::]:3900"
 root_domain = ".s3.garage.localhost"
 
+[s3_web]
+bind_addr = "[::]:3902"
+root_domain = ".web.garage.localhost"
+index = "index.html"
+
+[k2v_api]
+api_bind_addr = "[::]:3904"
+
 [admin]
 api_bind_addr = "[::]:3903"
 admin_token = "$(openssl rand -base64 32)"
