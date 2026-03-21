1e34a0af2b45848f2eb9badf03f16b02f97eb934
Jonas Pohl <git@jaypi4c.de>
2026-03-21 14:43:48 +0100

    Updating deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+docker model pull ai/mxbai-embed-large
+docker model pull ai/smollm2
+
 mkdir -p data
 
 curl -o data/createGarageToml.sh https://raw.githubusercontent.com/uol-esis/civicsage/33-add-independent-install-script/deployment/data/createGarageToml.sh
@@ -17,5 +20,7 @@ echo $'CIVICSAGE_S3_SECRETKEY=<replace me>\nCIVICSAGE_S3_ACCESSKEY=<replace me>'
  
 docker compose up -d garage garage_config db frontend && docker compose logs -f garage_config
 
+echo "Please add the generated keys to the .env file"
+read -p "Press enter to continue after configuration"
 
-
+docker compose up -d backend
