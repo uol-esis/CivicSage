63a4b19145116aa6068033be453d0f51a45f1c5f
Jonas Pohl <git@jaypi4c.de>
2026-03-21 14:06:03 +0100

    more updates

--- frontend/js-client/generate.sh
+++ frontend/js-client/generate.sh
@@ -4,9 +4,15 @@ GID=$(id -g)
 docker run --rm \
   -u "${UID}":"${GID}" \
   -v "${PWD}":/local \
-  -v ../../openapi/openapi.yaml:/input/openapi.yaml \
+  -v ../../backend/openapi/openapi.yaml:/input/openapi.yaml \
    openapitools/openapi-generator-cli:v7.13.0 generate \
   -i /input/openapi.yaml \
   -g javascript \
   -t /local/templates \
   -o /local
+
+docker run --rm \
+  -u "${UID}":"${GID}" \
+  -v "${PWD}":/local \
+  node:24.14-alpine \
+  sh -c "cd /local && npm install && npm run build && npm pack"
