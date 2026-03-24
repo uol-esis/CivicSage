7c29be6bd7f4f40d2b135c69f6135f07bf1fcce0
Jonas Pohl <git@jaypi4c.de>
2026-03-24 13:45:17 +0100

    more updates

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
+
+if ! command -v curl >/dev/null 2>&1
+then
+    echo "curl could not be found" >&2
+    exit 1
+fi
+
+if ! command -v docker >/dev/null 2>&1
+then
+    echo "docker could not be found" >&2
+    exit 1
+fi
+if ! command -v docker >/dev/null 2>&1 || ! docker compose version >/dev/null 2>&1
+then
+    echo "Error: 'docker compose' is not available" >&2
+    echo "Please install Docker with compose plugin" >&2
+    exit 1
+fi
+
+if ! command -v docker >/dev/null 2>&1 || ! docker model version >/dev/null 2>&1
+then
+    echo "Error: 'docker model' is not available" >&2
+    echo "Please install Docker with model plugin" >&2
+    exit 1
+fi
+
 
 docker model pull ai/mxbai-embed-large
 docker model pull ai/smollm2
@@ -9,7 +36,7 @@ mkdir -p data
 
 curl -o data/createGarageToml.sh https://raw.githubusercontent.com/uol-esis/civicsage/main/deployment/data/createGarageToml.sh
 chmod +x data/createGarageToml.sh
-data/createGarageToml.sh
+./data/createGarageToml.sh
 
 curl -o data/configure.sh https://raw.githubusercontent.com/uol-esis/civicsage/main/deployment/data/configure.sh
 chmod +x data/configure.sh
@@ -17,12 +44,13 @@ chmod +x data/configure.sh
 curl -o docker-compose.yaml https://raw.githubusercontent.com/uol-esis/civicsage/main/deployment/docker-compose.yaml
 
 touch .env
-echo $'CIVICSAGE_S3_SECRETKEY=<replace me>\nCIVICSAGE_S3_ACCESSKEY=<replace me>' >> .env
- 
+printf 'CIVICSAGE_S3_SECRETKEY=<replace me>\nCIVICSAGE_S3_ACCESSKEY=<replace me>\n' >> .env
+
 docker compose up -d garage garage_config db frontend && docker compose logs -f garage_config
 
 echo "Please add the generated keys to the .env file"
-read -p "Press enter to continue after configuration"
+printf "Press enter to continue after configuration"
+read dummy
 
 docker compose up -d backend
 
